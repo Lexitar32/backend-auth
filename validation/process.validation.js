2b7d73a7cbc0c2fea1a00f250590cb19b718ee5f
Arimoro Olamilekan <arimoroolamilekan1@gmail.com>
2022-03-16 16:08:29 +0100

    fix(process-description): made process description not compulsory

--- validation/process.validation.js
+++ validation/process.validation.js
@@ -1,38 +1,38 @@
 const Joi = require("joi");
 
 exports.createProcessValidation = () => {
-    const schema = Joi.object({
-        processName: Joi.string()
-            .required()
-            .error(new Error("Process Name is required!")),
-        // description: Joi.string()
-        //     .min(20)
-        //     .max(100)
-        //     .error(
-        //         new Error(
-        //             "Process Description should be min of 20 characters and max of 100 characters"
-        //         )
-        //     ),
-        userId: Joi.string().required().error(new Error("User Id is required")),
-    });
+  const schema = Joi.object({
+    processName: Joi.string()
+      .required()
+      .error(new Error("Process Name is required!")),
+    description: Joi.string(),
+    // .min(20)
+    // .max(100)
+    // .error(
+    //     new Error(
+    //         "Process Description should be min of 20 characters and max of 100 characters"
+    //     )
+    // ),
+    userId: Joi.string().required().error(new Error("User Id is required")),
+  });
 
-    return schema;
+  return schema;
 };
 
 exports.updateProcessValidation = () => {
-    const schema = Joi.object({
-        processName: Joi.string()
-            .required()
-            .error(new Error("Process Name is required!")),
-        // description: Joi.string()
-        //     .min(20)
-        //     .max(100)
-        //     .error(
-        //         new Error(
-        //             "Process Description should be min of 20 characters and max of 100 characters"
-        //         )
-        //     ),
-    });
+  const schema = Joi.object({
+    processName: Joi.string()
+      .required()
+      .error(new Error("Process Name is required!")),
+    description: Joi.string(),
+    //     .min(20)
+    //     .max(100)
+    //     .error(
+    //         new Error(
+    //             "Process Description should be min of 20 characters and max of 100 characters"
+    //         )
+    //     ),
+  });
 
-    return schema;
+  return schema;
 };
