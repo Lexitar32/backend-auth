aa9c7dbe5e6c87d1399b289a174bf6db14c91ae6
Arimoro Olamilekan <arimoroolamilekan1@gmail.com>
2022-03-31 13:09:05 +0100

    feat(tasks): built the api for saving and fetching task details

--- controller/textEditor.controller.js
+++ controller/textEditor.controller.js
@@ -1,21 +1,33 @@
 const Step = require("../models/steps.model");
+const taskList = require("../models/taskList.model")
 const textEditorModel = require("../models/textEditor.model");
 
 exports.createStepDesc = async (req, res) => {
     try {
         const data = {};
-        const { stepDesc, stepId } = req.body;
+        const { stepDesc, stepId, taskId } = req.body;
 
         data.stepDesc = stepDesc;
         data.stepId = stepId;
+        data.taskId = taskId;
 
         const textEditor = new textEditorModel(data);
         await textEditor.save();
-        await Step.findOneAndUpdate(
-            { _id: data.stepId },
-            { $push: { stepDesc: textEditor._id } },
-            { new: true }
-        );
+
+        if(data.stepId) {
+            await Step.findOneAndUpdate(
+                { _id: data.stepId },
+                { $push: { stepDesc: textEditor._id } },
+                { new: true }
+            );
+        } else {
+            await taskList.findOneAndUpdate(
+                { _id: data.taskId },
+                { $push: { stepDesc: textEditor._id } },
+                { new: true }
+            );
+        }
+        
         res.send({
             message: "Step Description Sucessfully Saved",
         });
--- models/textEditor.model.js
+++ models/textEditor.model.js
@@ -2,26 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const textEditorSchema = new Schema(
-    {
-        stepDesc: {
-            type: String,
-        },
-        stepId: {
-            type: String,
-            required: true
-        }
+  {
+    stepDesc: {
+      type: String,
     },
-    {
-        timestamps: true,
-    }
+    stepId: {
+      type: String,
+    },
+    taskId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 textEditorSchema.methods.toJSON = function () {
-    const stepDesc = this;
-    const textEditorObject = stepDesc.toObject();
+  const stepDesc = this;
+  const textEditorObject = stepDesc.toObject();
 
-    delete textEditorObject.__v;
-    return textEditorObject;
+  delete textEditorObject.__v;
+  return textEditorObject;
 };
 
 const textEditor = mongoose.model("textEditor", textEditorSchema);
