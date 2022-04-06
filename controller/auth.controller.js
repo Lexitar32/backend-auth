c6190e605652d51c406c30cfc55a3fe56cbe23e9
Arimoro Olamilekan <arimoroolamilekan1@gmail.com>
2022-04-07 00:52:27 +0100

    feat(authentication): apis for sign up, login, logout, update user profile and submitting of survey done

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -50,10 +50,7 @@ exports.emailVerification = async (req, res) => {
     );
 
     if (!response) throw new Error("This user is not registered");
-    res.status(201).send({
-      message: "Email Verified",
-    });
-    // res.redirect(process.env.LOGIN_REDIRECT);
+    res.redirect(process.env.LOGIN_REDIRECT);
   } catch (error) {
     res.status(400).send({
       error: error.message,
