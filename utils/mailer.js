591e8d34e9dad8063528873afb93300e91bfbde9
Arimoro Olamilekan <arimoroolamilekan1@gmail.com>
2022-04-09 14:38:56 +0100

    feat(email-verification): email verification implemented with workover sendgrid account and send grid email template

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,20 +3,22 @@ const { createAccessToken } = require("../tokens/index");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.emailVerification = (user) => {
-    const token = createAccessToken(user);
-    const msg = {
-        to: user.email,
-        from: "olamilekan@scalemath.com",
-        subject: "Workover Platform",
-        text: "and easy to do anywhere, even with Node.js",
-        html: `Thanks for registering on Workover platform, we are happy to have you here, Kindly Click <a href="${process.env.BASE_URL}/auth/confirm-email?token=${token}">here</a> to very your email`,
-    };
-    sgMail
-        .send(msg)
-        .then(() => {
-            console.log("Email sent");
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  const token = createAccessToken(user);
+  const msg = {
+    to: user.email,
+    from: "support@workover.io",
+    subject: "Workover Platform",
+    templateId: "d-a14734ba8a09451c8e2def97b149b28b",
+    dynamicTemplateData: {
+      verifylink: `${process.env.BASE_URL}/auth/confirm-email?token=${token}`,
+    }
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
