375657303e039c0e8d19c4962a2305e6bd1fee98
Arimoro Olamilekan <arimoroolamilekan1@gmail.com>
2022-04-12 14:14:22 +0100

    feat(api-adjustment): adjusted authentication

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.static("public"));
-app.use(cors({ origin: ["http://localhost:3000", "http://app.workover.io"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "https://app.workover.io"], credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
